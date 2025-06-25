0fb1016c5505f5d74d9c52cdd55ad897a9cf6579
Skakarla2023 <skakarla2023@gmail.com>
2025-06-25 22:00:25 +0530

    Update Kadane's_Algorithm.java

--- 53/Kadane's_Algorithm.java
+++ 53/Kadane's_Algorithm.java
@@ -4,16 +4,14 @@ public class Maximum_subarray_sum_Kadanes_Algorithm
 {
 	public static void main(String[] args)
 	{
+		int max_sum=Integer.MIN_VALUE;
 		int[] arr= {3,-1,2,-6,7,8,-4,9};
 		int n=arr.length;
 		int curr_sum=0;
 		for(int i=0;i<n;i++)
 		{
-			curr_sum+=arr[i];
-			if(curr_sum<0)
-			{
-				curr_sum=0;
-			}
+			curr_sum=Math.max(arr[i],curr_sum+arr[i]);
+			max_sum=Math.max(curr_sum, max_sum);
 		}
 		System.out.print(curr_sum);
 	}
