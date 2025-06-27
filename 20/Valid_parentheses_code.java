f63369eea435b24fc8e7a5ea58bc942ba24d1bcb
Skakarla2023 <skakarla2023@gmail.com>
2025-06-27 06:41:10 +0530

    Create Valid_parentheses_code.java

---
+++ 20/Valid_parentheses_code.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String a) {
+        Stack<Character> str = new Stack<>();
+		for (int i = 0; i < a.length(); i++) {
+			char ch = a.charAt(i);
+			if (ch == '(' || ch == '{' || ch == '[') {
+				str.push(ch);
+			} else {
+				if (str.isEmpty()) {
+					return false;
+				}
+				char top = str.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+			}
+		}
+
+		return str.isEmpty();
+    }
+}
