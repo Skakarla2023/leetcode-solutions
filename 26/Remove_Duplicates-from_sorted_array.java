f7c516f2d0e59e754504f2ae16fecef3a02703b3
Skakarla2023 <skakarla2023@gmail.com>
2025-07-11 06:44:40 +0530

    Update Remove_Duplicates-from_sorted_array.java

--- 26/Remove_Duplicates-from_sorted_array.java
+++ 26/Remove_Duplicates-from_sorted_array.java
@@ -5,24 +5,20 @@ public class Remove_Duplcates_from_sorted_Array {
 	public static void main(String[] args) {
 		int[] nums = { 0, 1, 1, 2, 3, 3, 4 };
 		int n = nums.length;
-		int count = 0;
-		for (int i = 0; i < n - 1; i++) {
-			if (nums[i] != nums[i + 1]) {
-				count++;
+		int i = 0; // pointer to update the array elements
+		// here we are going to use another pointer j to traverse through the array
+		// elements
+		for (int j = 1; j < n; j++) {
+			if(nums[j]!=nums[i]) {
+				i++;
+				nums[i]=nums[j];
 			}
 		}
-		count += 1;
-		int[] unique = new int[count];
-		unique[0] = nums[0];
-		int idx = 1;
-		for (int i = 1; i < n; i++) {
-			if (nums[i] != nums[i - 1]) {
-				unique[idx++] = nums[i];
-			}
-		}
-
-		for (int i : unique) {
-			System.out.println(i);
+		int newlength=i+1;
+		System.out.println("Length of the updated array:"+newlength);
+		
+		for(int a=0;a<newlength;a++) {
+			System.out.println(nums[a]);
 		}
 	}
 
