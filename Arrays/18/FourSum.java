1d6b0202197a7f440e28dfcaeeb97f2f9649afe8
Skakarla2023 <skakarla2023@gmail.com>
2025-11-28 05:00:03 +0530

    Update FourSum.java

--- Arrays/18/FourSum.java
+++ Arrays/18/FourSum.java
@@ -6,12 +6,49 @@ public class FourSum {
 
 	public static void main(String[] args) {
 		int[] nums = { -2, 4, 2, -1, 0, 1, 1, -1 };
+		Arrays.sort(nums);
+		int target = 0;
 
-		System.out.println(foursum(nums));
+		System.out.println(foursum(nums, target));
 	}
 
-	private static List<List<Integer>> foursum(int[] nums) {
+	private static List<List<Integer>> foursum(int[] nums, int target) {
 		List<List<Integer>> result = new ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (i > 0 && nums[i] == nums[i - 1]) {
+				continue;
+			}
+			for (int j = i + 1; j < nums.length; j++) {
+				if (j > i + 1 && nums[j] == nums[j - 1]) {
+					continue;
+				}
+				int left = j + 1;
+				int right = nums.length - 1;
+
+				while (left < right) {
+					int sum = nums[i] + nums[j] + nums[left] + nums[right];
+					if (sum == target) {
+						result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+						left++;
+						right--;
+
+						while (left < right && nums[left] == nums[left + 1]) {
+							left++;
+						}
+
+						while (left < right && nums[right] == nums[right - 1]) {
+							right--;
+						}
+					} else if (sum < 0) {
+						left++;
+					} else {
+						right--;
+					}
+				}
+			}
+		}
 		return result;
 	}
 
