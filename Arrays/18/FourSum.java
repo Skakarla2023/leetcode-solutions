1b0fa9d23fa0acc24d95f454e2b00789227ccc0f
Skakarla2023 <skakarla2023@gmail.com>
2025-11-27 06:02:24 +0530

    Create FourSum.java

---
+++ Arrays/18/FourSum.java
@@ -0,0 +1,18 @@
+package leetcode_programs_updated;
+
+import java.util.*;
+
+public class FourSum {
+
+	public static void main(String[] args) {
+		int[] nums = { -2, 4, 2, -1, 0, 1, 1, -1 };
+
+		System.out.println(foursum(nums));
+	}
+
+	private static List<List<Integer>> foursum(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		return result;
+	}
+
+}
