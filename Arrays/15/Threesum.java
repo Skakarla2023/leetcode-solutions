fbe627c0897aefc18273d6067f97a9f7a31dba08
Skakarla2023 <skakarla2023@gmail.com>
2025-11-27 05:39:37 +0530

    Create Threesum.java

---
+++ Arrays/15/Threesum.java
@@ -0,0 +1,56 @@
+package leetcode_programs_updated;
+
+import java.util.*;
+
+public class ThreeSum {
+
+	public static void main(String[] args) {
+
+		int[] nums = { -2, 4, 2, -1, 0, 1, 1, -1 };
+
+		System.out.println(threeSum(nums));
+
+	}
+
+	private static List<List<Integer>> threeSum(int[] nums) {
+
+		List<List<Integer>> result = new ArrayList<>();
+
+		int n = nums.length;
+
+		for (int i = 0; i < n - 2; i++) {
+
+			int left = i + 1;
+			int right = n - 1;
+
+			if (i > 0 && nums[i] == nums[i - 1]) {
+				continue;
+			}
+
+			while (left < right) {
+				int sum = nums[i] + nums[left] + nums[right];
+				if (sum == 0) {
+					result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+					left++;
+					right--;
+
+					while (left < right && nums[left] == nums[left - 1]) {
+						left++;
+					}
+
+					while (left < right && nums[right] == nums[right + 1]) {
+						right--;
+					}
+				} else if (sum < 0) {
+					left++;
+				} else {
+					right--;
+				}
+			}
+
+		}
+
+		return result;
+	}
+
+}
