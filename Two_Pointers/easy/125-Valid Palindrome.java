8178cfde080d490b5533cba65ff13db90c1bff97
Skakarla2023 <skakarla2023@gmail.com>
2026-02-07 12:12:24 +0530

    Update 125-Valid Palindrome.java

--- Two_Pointers/easy/125-Valid Palindrome.java
+++ Two_Pointers/easy/125-Valid Palindrome.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int start = 0, end = str.length()-1;
+        while(start<end){
+            if(str.charAt(start)==str.charAt(end)){
+                start++;
+                end--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
