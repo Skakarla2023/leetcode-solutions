8c7b36b143a21f74ef137428f9ee0b9b5eb05714
Skakarla2023 <skakarla2023@gmail.com>
2026-02-26 06:12:42 +0530

    Create 18. 4Sum.java

---
+++ Two_Pointers/medium/18. 4Sum.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
+                        List<Integer> subset = new ArrayList<>();
+                        subset.add(nums[i]);
+                        subset.add(nums[j]);
+                        subset.add(nums[k]);
+                        subset.add(nums[l]);
+                        list.add(subset);
+
+                        k++;
+                        l--;
+
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
+                    }
+
+                    else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+
+            }
+        }
+        return list;
+    }
+}
