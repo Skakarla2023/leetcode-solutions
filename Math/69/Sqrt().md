10a3c0ba7ab4d6c6224ed99a432c3248e247c622
Skakarla2023 <skakarla2023@gmail.com>
2026-02-27 07:34:08 +0530

    Delete Math directory

--- Math/69/Sqrt().md
+++
@@ -1,1 +0,0 @@
-<img width="1759" height="650" alt="image" src="https://github.com/user-attachments/assets/b5a78926-e344-4fb7-b12f-1373dd6d602a" />
--- Math/69/Sqrt.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int mySqrt(int x) {
-        double num = Math.pow(x,0.5);
-        int res = (int)num;
-        return res;
-    }
-}
