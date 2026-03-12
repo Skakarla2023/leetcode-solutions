101c2189be9ee29c670362b63a196e34b342eb7b
Skakarla2023 <skakarla2023@gmail.com>
2026-03-12 05:38:49 +0530

    Create Brute_Force.java

---
+++ Hashing/easy/1.Two Sum/Brute_Force.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int first = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                int second = nums[j];
+                int sum = first + second;
+                if (target == sum) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { 0, 0 };
+    }
+}
