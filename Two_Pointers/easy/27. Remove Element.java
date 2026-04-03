1d767452ae31d610bdf3661c0aa87902e087554f
Skakarla2023 <skakarla2023@gmail.com>
2026-04-03 12:49:02 +0530

    Update 27. Remove Element.java

--- Two_Pointers/easy/27. Remove Element.java
+++ Two_Pointers/easy/27. Remove Element.java
@@ -1,34 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count = 0;
-        int start=0,end=nums.length-1;
-        while(start<end){
-            if(nums[start]==val){
-                if(nums[end]!=val){
-                    int temp = nums[start];
-                    nums[start] = nums[end];
-                    nums[end] = temp;
-                    start++;
-                    end--;
-                }
-                else{
-                    end--;
-                }
-            }
-            else{
+        int n = nums.length;
+        int start = 0, end = n - 1;
+
+        if (n == 1 && nums[n - 1] == val) {
+            return 0;
+        }
+
+        while (start <= end) {
+            if (nums[start] == val) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                end--;
+            } else {
                 start++;
             }
         }
-        int i=0;
-        while(i<nums.length){
-            if(nums[i]!=val){
-                count++;
-                i++;
-            }
-            else{
-                i++;
-            }
+
+        if (start == end - 1) {
+            return 0;
+        } else {
+            return start;
         }
-        return count;
     }
 }
