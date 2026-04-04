d64b65d944b83e0050ab88d9939349be74102c0b
Skakarla2023 <skakarla2023@gmail.com>
2026-04-04 07:55:10 +0530

    Create 169. Majority Element.java

---
+++ Hashing/easy/169. Majority Element.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int max = 0;
+        for (int i : map.keySet()) {
+            if (map.get(i) > n / 2) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
