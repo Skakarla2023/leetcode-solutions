5c7013f22340f98af4ed0f5be18c55e45fc22393
Skakarla2023 <skakarla2023@gmail.com>
2026-05-11 04:49:06 +0530

    Update Solution.java

--- Hashing/easy/242. Valid Anagram/Solution.java
+++ Hashing/easy/242. Valid Anagram/Solution.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        if(t.length() != s.length()) {
             return false;
         }
 
-        HashMap<Character, Integer> map = new HashMap<>();
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
@@ -14,8 +15,9 @@ class Solution {
         for (char c : t.toCharArray()) {
             if (!map.containsKey(c)) {
                 return false;
+            } else {
+                map.put(c, map.get(c) - 1);
             }
-            map.put(c, map.get(c) - 1);
 
             if (map.get(c) == 0) {
                 map.remove(c);
