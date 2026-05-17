c1fd4e5aa555f2efa274ee77a9d9d68820591ead
Skakarla2023 <skakarla2023@gmail.com>
2026-05-17 14:30:00 +0530

    Create Solution.java

---
+++ Two_Pointers/easy/125. Valid Palindrome/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String str) {
+        int n = str.length();
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            while (start < end && !Character.isLetterOrDigit(str.charAt(start))) {
+                start++;
+            }
+            while (start < end && !Character.isLetterOrDigit(str.charAt(end))) {
+                end--;
+            }
+
+            if (Character.toLowerCase(str.charAt(start)) != Character.toLowerCase(str.charAt(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
