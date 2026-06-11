308348d03e016dacdea6714c83da8b6c85b2d87a
Skakarla2023 <skakarla2023@gmail.com>
2026-06-11 05:48:54 +0530

    Create Solution.java

---
+++ z_Data Structures/LinkedList/easy/83. Remove Duplicates from Sorted List/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        while (curr.next != null) {
+            if (curr.val != curr.next.val) {
+                curr = curr.next;
+            } else {
+                curr.next = curr.next.next;
+            }
+        }
+
+        return head;
+
+    }
+}
