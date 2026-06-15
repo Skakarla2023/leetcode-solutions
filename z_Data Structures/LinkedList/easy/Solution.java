217341e9dd7dcd20175fb74c43b337648a166feb
Skakarla2023 <skakarla2023@gmail.com>
2026-06-15 16:36:52 +0530

    Create Solution.java

---
+++ z_Data Structures/LinkedList/easy/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode pA, ListNode pB) {
+        ListNode headA = pA;
+        ListNode headB = pB;
+
+        while(pA != pB) {
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+
+
+        return pA;
+    }
+}
