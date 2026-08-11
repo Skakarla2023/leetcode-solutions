e19f963e6f33289d2f9f552ccdcf95fe04209c68
Satwika Kakarla <skakarla2023@gmail.com>
2026-08-11 06:29:02 +0530

    Create Solution.java

---
+++ Dynamic Programming/easy/338. Counting Bits/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+
+        if(n==0) {
+            return new int[]{0};
+        }
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i & (i - 1)] + 1;
+        }
+
+        return dp;
+    }
+}
